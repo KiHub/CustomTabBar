f2406b6121b0d84927a9f344890755e7afb8bd54
Kirill Ki <kirillkalapov@gmail.com>
2022-06-04 13:19:40 +0200

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by  Mr.Ki on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
